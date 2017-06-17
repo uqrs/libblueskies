a95478111610703af23b33276ebdc69cf1837f35
Patternsheep <meesrudigerard@live.nl>
2017-06-18 01:33:52 +0200

    Properly elaborated on the KTN table. Added fields for KSN.

--- src/indices.lua
+++ src/indices.lua
@@ -15,6 +15,11 @@
 -- If the indexed field ends in '_raw', then the 'COPY' handler will be used- i.e. the metamethod doesn't process the data-
 -- instead, it will hand it back in its entirety.
 --
+-- Down below, for the subtables in the META table, a few special rules apply:
+--		If either the LENGTH or OFFSET is a string, then the respective contents of the field the string describes will be used.
+--			e.g. LENGTH of field `A` is "B", then `B`s length will be used.
+--		If OFFSET/LENGTH is nil, then the metamethod will attempt to guess these.
+--
 -- [The reference tables found in this file are registered under a Creative Commons Attribution-ShareAlike 4.0 International
 --  License, and it authored by and can be accredited to the Flipnote Collective. Nothing but the wording has been slightly
 --  altered; the addition of the "IDENTIFIER" column has been introduced by libblueskies and was not at all included by the
@@ -57,36 +62,76 @@ local UTF16=HANDLERS.UTF16()
 -- 211     1            Layer Visibility               ???                                    layer_vis
 --------------------------------------------------------------------------------------------------------------------------------
 META.KFH={
--- IDENTIFIER    OFFSET    LENGTH  HANDLER
-  header       ={0,        8,      COPY          },
-  crc32        ={8,        4,      COPY          },
-  creation     ={12,       4,      EPOCH_2000    },
-  last_edit    ={16,       4,      EPOCH_2000    },
-  unknown      ={20,       4,      COPY          },
-  creator_id   ={24,       10,     BIG_ENDIAN    },
-  parent_id    ={34,       10,     BIG_ENDIAN    },
-  current_id   ={44,       10,     BIG_ENDIAN    },
-  creator_name ={54,       22,     UTF16         },
-  parent_name  ={76,       22,     UTF16         },
-  current_name ={98,       22,     UTF16         },
-  root_file    ={120,      22,     COPY          },
-  parent_file  ={148,      22,     COPY          },
-  current_file ={176,      22,     COPY          },
-  frames       ={204,      2,      LITTLE_ENDIAN },
-  thumbnail    ={206,      2,      LITTLE_ENDIAN },
-  flags        ={208,      2,      COPY          },
-  framerate    ={210,      1,      COPY          },
-  layer_vis    ={211,      1,      COPY          }
+-- IDENTIFIER     OFFSET    LENGTH            HANDLER
+   header       ={0,        8,                COPY          },
+   crc32        ={8,        4,                COPY          },
+   creation     ={12,       4,                EPOCH_2000    },
+   last_edit    ={16,       4,                EPOCH_2000    },
+   unknown      ={20,       4,                COPY          },
+   creator_id   ={24,       10,               BIG_ENDIAN    },
+   parent_id    ={34,       10,               BIG_ENDIAN    },
+   current_id   ={44,       10,               BIG_ENDIAN    },
+   creator_name ={54,       22,               UTF16         },
+   parent_name  ={76,       22,               UTF16         },
+   current_name ={98,       22,               UTF16         },
+   root_file    ={120,      22,               COPY          },
+   parent_file  ={148,      22,               COPY          },
+   current_file ={176,      22,               COPY          },
+   frames       ={204,      2,                LITTLE_ENDIAN },
+   thumbnail    ={206,      2,                LITTLE_ENDIAN },
+   flags        ={208,      2,                COPY          },
+   framerate    ={210,      1,                COPY          },
+   layer_vis    ={211,      1,                COPY          }
 }
 --------------------------------------------------------------------------------------------------------------------------------
 -- Thumbnail Section -- CC-BY-SA 4.0 applies to this comment block.
 --------------------------------------------------------------------------------------------------------------------------------
+-- OFFSET  LENGTH       CONTENT                        NOTES                                  IDENTIFIER
+-- 0       8            Section Header                                                        header
+-- 8       4            Unknown                                                               unknown
+-- 12      4            JPG Image Data                 80x64px                                jpg
+--------------------------------------------------------------------------------------------------------------------------------
+-- Need to figure out how to turn the JPG Image data into something sensible.
+--------------------------------------------------------------------------------------------------------------------------------
 META.KTN={
--- IDENTIFIER    OFFSET    LENGTH  HANDLER
-  header       ={0,        8,      COPY          },
-  unknown      ={8,        4,      COPY          },
-  thumbnail    ={12,       4,      COPY          }
+-- IDENTIFIER     OFFSET    LENGTH            HANDLER
+   header       ={0,        8,                COPY          },
+   unknown      ={8,        4,                COPY          },
+   jpg          ={12,       4,                COPY          }
 }
+--------------------------------------------------------------------------------------------------------------------------------
+-- Sound Section -- CC-BY-SA 4.0 applies to this comment block.
+--------------------------------------------------------------------------------------------------------------------------------
+-- OFFSET  LENGTH       CONTENT                        NOTES                                  IDENTIFIER
+-- 0       8            Section Header                                                        header
+-- 8       4            Speed When Recorded                                                   speed
+-- 12      4            Background Music Length        Unsigned Little Endian                 bgm_length
+-- 16      4            Sound Effect 1 (A) Length      Ditto.                                 se1_length
+-- 20      4            Sound Effect 2 (X) Length      Ditto.                                 se2_length
+-- 24      4            Sound Effect 3 (Y) Length      Ditto.                                 se3_length
+-- 28      4            Sound Effect 4 (UP)Length      Ditto.                                 se4_length
+-- 32      bgm_length   Background Music Data          4-bit ADPCM @ 16.5kHz                  bgm_data
+--         se1_length   Sound Effect 1 (A) Data        Ditto.                                 se1_data
+--         se2_length   Sound Effect 2 (X) Data        Ditto.                                 se2_data
+--         se3_length   Sound Effect 3 (Y) Data        Ditto.                                 se3_data
+--         se4_length   Sound Effect 4 (UP)Data        Ditto.                                 se4_data
+--------------------------------------------------------------------------------------------------------------------------------
+META.KSN={
+-- IDENTIFIER     OFFSET    LENGTH            HANDLER
+   header       ={0,        8,                COPY          },
+   speed        ={8,        4,                COPY          },
+   bgm_length   ={12,       4,                LITTLE_ENDIAN },
+   se1_length   ={16,       4,                LITTLE_ENDIAN },
+   se2_length   ={20,       4,                LITTLE_ENDIAN },
+   se3_length   ={24,       4,                LITTLE_ENDIAN },
+   se4_length   ={28,       4,                LITTLE_ENDIAN },
+   bgm_data     ={32,       "bgm_length",     COPY          },
+   se1_data     ={nil,      "se1_length",     COPY          },
+   se2_data     ={nil,      "se2_length",     COPY          },
+   se3_data     ={nil,      "se3_length",     COPY          },
+   se4_data     ={nil,      "se4_length",     COPY          }
+}
+
 --------------------------------------------------------------------------------------------------------------------------------
 -- Binding Function
 --------------------------------------------------------------------------------------------------------------------------------
